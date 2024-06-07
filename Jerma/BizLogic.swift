9203c6a4e84dc402c23dc6fdbf9ba01e83f2d018
Patryk <me@parpok.xyz>
2024-06-07 15:02:46 +0200

    Add picker for the models. Make the app look more simmilar to target.

--- Jerma/BizLogic.swift
+++ Jerma/BizLogic.swift
@@ -9,25 +9,35 @@ import Foundation
 import GoogleGenerativeAI
 
 enum APIKey {
-  // Fetch the API key from `GenerativeAI-Info.plist`
-  static var `default`: String {
-      guard let filePath = Bundle.main.path(forResource: "GenerativeAI-Info", ofType: "plist")
-      else {
-        fatalError("Couldn't find file 'GenerativeAI-Info.plist'.")
-      }
-      let plist = NSDictionary(contentsOfFile: filePath)
-      guard let value = plist?.object(forKey: "API_KEY") as? String else {
-        fatalError("Couldn't find key 'API_KEY' in 'GenerativeAI-Info.plist'.")
-      }
-      if value.starts(with: "_") {
-        fatalError(
-          "Follow the instructions at https://ai.google.dev/tutorials/setup to get an API key."
-        )
-      }
-      return value
-  }
+    // Fetch the API key from `GenerativeAI-Info.plist`
+    static var `default`: String {
+        guard let filePath = Bundle.main.path(forResource: "GenerativeAI-Info", ofType: "plist")
+        else {
+            fatalError("Couldn't find file 'GenerativeAI-Info.plist'.")
+        }
+        let plist = NSDictionary(contentsOfFile: filePath)
+        guard let value = plist?.object(forKey: "API_KEY") as? String else {
+            fatalError("Couldn't find key 'API_KEY' in 'GenerativeAI-Info.plist'.")
+        }
+        if value.starts(with: "_") {
+            fatalError(
+                "Follow the instructions at https://ai.google.dev/tutorials/setup to get an API key."
+            )
+        }
+        return value
+    }
+}
+
+enum ModelsAvailble: String, CaseIterable, Identifiable {
+    case gemini_1_5_flash = "gemini-1.5-flash"
+    case gemini_1_5_pro = "gemini-1.5-pro"
+    case gemini_1_pro = "gemini-1.0-pro"
+
+    var id: Self {
+        return self
+    }
 }
 
 // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
 // Access your API key from your on-demand resource .plist file (see "Set up your API key" above)
-let model = GenerativeModel(name: "gemini-1.5-flash", apiKey: APIKey.default)
+let model = GenerativeModel(name: String(ModelsAvailble.gemini_1_5_flash.rawValue), apiKey: APIKey.default)
--- Jerma/ContentView.swift
+++ Jerma/ContentView.swift
@@ -9,53 +9,84 @@ import GoogleGenerativeAI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var question: String = ""
-    @State private var question2: String = ""
+    @State private var Question: String = ""
+    @State private var Question2: String = ""
     @State private var FinalResponse: String = ""
+    @State private var PickedModel: ModelsAvailble = ModelsAvailble.gemini_1_5_flash
 
     var body: some View {
         VStack {
             NavigationStack {
                 VStack {
                     VStack {
-                        if !question2.isEmpty {
-                            Text("You: \(question2)")
-                                .multilineTextAlignment(.leading)
-                                .font(.headline)
+                        ScrollView {
+                            if !Question2.isEmpty {
+                                Text("You: \(Question2)")
+                                    .multilineTextAlignment(.leading)
+                                    .font(.headline)
+                            }
+                            Spacer()
+
+                            if !FinalResponse.isEmpty {
+                                Text("Ai says: \(FinalResponse)")
+                                    .multilineTextAlignment(.leading)
+                                    .font(.subheadline)
+                                    .defaultScrollAnchor(.center)
+                            }
                         }
-                        Spacer()
 
-                        Text(FinalResponse)
-                            .multilineTextAlignment(.leading)
-                            .font(.subheadline)
-
-                    }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading).textSelection(.enabled).padding()
                     VStack {
                         HStack {
                             Button(action: { print("Add things") }, label: {
-                                Image(systemName: "plus.app")
-                            })
+                                Image(systemName: "plus.square")
+                            }).buttonStyle(.bordered)
 
-                            TextField("AiQuestionBox", text: $question, prompt: Text("Ask it")).textFieldStyle(.roundedBorder)
+                            TextField("AiQuestionBox", text: $Question, prompt: Text("Ask \(PickedModel.rawValue) something"), axis: .vertical).textFieldStyle(.roundedBorder)
                             Button {
-                                question = question2
+                                Question2 = Question
                                 Task {
-                                    let response = try await model.generateContent(question)
+                                    let response = try await model.generateContent(Question)
                                     if let text = response.text {
                                         print(text)
                                         FinalResponse = text
                                     }
+                                    Question = ""
                                 }
                             } label: {
                                 Image(systemName: "paperplane")
-                            }
+                            }.buttonStyle(.borderedProminent)
                         }
-                    }.font(.title).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    }.imageScale(.large).buttonBorderShape(.roundedRectangle).frame(maxWidth: .infinity, alignment: .bottom).padding(EdgeInsets(top: 0, leading: 2.5, bottom: 20, trailing: 2.5))
                 }.toolbar {
                     ToolbarItem(placement: .topBarLeading) {
                         Button(action: { print("Show history") }, label: {
-                            Image(systemName: "list.bullet")
+                            Label(
+                                title: { Text("Show history") },
+                                icon: { Image(systemName: "list.bullet") }
+                            )
                         })
+                    }
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Menu {
+                            ForEach(ModelsAvailble.allCases) { model in
+                                Button {
+                                    PickedModel = model
+                                } label: {
+                                    if model == PickedModel {
+                                        Image(systemName: "checkmark")
+                                        Text(model.rawValue)
+                                    } else {
+                                        Text(model.rawValue)
+                                    }
+                                }
+                            }
+                        } label: {
+                            Label(
+                                title: { Text("Choose model") },
+                                icon: { Image(systemName: "sparkles") }
+                            )
+                        }
                     }
                 }
                 .navigationTitle("Ai")
