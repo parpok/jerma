703418dff2ba66f9b32e436c14042498be4e5224
Patryk <me@parpok.xyz>
2024-06-13 21:16:43 +0200

    Kind of clueless of me to think that I can use multiple images with this thing. Whatever fixed that and it works now. But preview still fed up.

--- Jerma/ChatView.swift
+++ Jerma/ChatView.swift
@@ -14,7 +14,7 @@ struct ChatView: View {
 
     @State private var AIAnswer: String = ""
 
-    @State private var ChatInputImages = [Data]()
+    @State private var ChatInputImage = Data()
 
     @State private var UserPrompt: String = ""
     var body: some View {
@@ -29,17 +29,14 @@ struct ChatView: View {
                             .frame(maxWidth: .infinity, alignment: .topLeading)
                     }
 
-                    if !ChatInputImages.isEmpty {
+                    if !ChatInputImage.isEmpty {
                         ScrollView(.horizontal) {
                             HStack {
-                                ForEach(0 ..< ChatInputImages.count, id: \.self) {
-                                    i in
-                                    Image(uiImage: UIImage(data: ChatInputImages[i])!)
-                                        .resizable()
-                                        .scaledToFit()
+                                Image(uiImage: UIImage(data: ChatInputImage)!)
+                                    .resizable()
+                                    .scaledToFit()
 
-                                }.frame(maxWidth: .infinity, maxHeight: 100, alignment: .topLeading)
-                            }
+                            }.frame(maxWidth: .infinity, maxHeight: 100, alignment: .topLeading)
                         }
                     }
 
@@ -53,7 +50,7 @@ struct ChatView: View {
                     }
                 }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
 
-                AskAiView(UserQuestionSubmitted: $UserPrompt, Answer: $AIAnswer, ResultImagesSubmitted: $ChatInputImages)
+                AskAiView(UserQuestionSubmitted: $UserPrompt, Answer: $AIAnswer, ResultImageSubmitted: $ChatInputImage)
             }.navigationTitle("GermaAiChat")
                 .toolbar {
                     ToolbarItem(placement: .topBarTrailing) {
@@ -89,40 +86,36 @@ struct AskAiView: View {
 
     @Binding var Answer: String
 
-    @State private var ChosenImages = [PhotosPickerItem]()
-    @State private var ResultImages: [Data] = []
-    @Binding var ResultImagesSubmitted: [Data]
-
+    @State private var ChosenImage = [PhotosPickerItem]()
+    @State private var ResultImage: Data = Data()
+    @Binding var ResultImageSubmitted: Data
 
     var body: some View {
         VStack {
-            if !ResultImages.isEmpty {
+            if !ResultImage.isEmpty {
                 VStack {
-                    Text("^[Including \(ResultImages.count) images:](inflect: true)").foregroundStyle(.gray).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                    Text("Including image:").foregroundStyle(.gray).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
                     ScrollView(.horizontal) {
                         HStack {
-                            ForEach(0 ..< ResultImages.count, id: \.self) {
-                                i in
-                                Image(uiImage: UIImage(data: ResultImages[i])!)
-                                    .resizable()
-                                    .scaledToFit()
+                            Image(uiImage: UIImage(data: ResultImage)!)
+                                .resizable()
+                                .scaledToFit()
 
-                            }.frame(maxWidth: .infinity, maxHeight: 100)
-                        }
+                        }.frame(maxWidth: .infinity, maxHeight: 100)
                     }
                 }
             }
             HStack {
-                PhotosPicker(selection: $ChosenImages) {
+                PhotosPicker(selection: $ChosenImage) {
                     Image(systemName: "plus.square")
                 }.buttonStyle(.bordered)
-                    .onChange(of: ChosenImages) { _, newItems in
+                    .onChange(of: ChosenImage) { _, newItems in
                         Task {
-                            ResultImages.removeAll()
+                            ResultImage.removeAll()
 
                             for item in newItems {
                                 if let image = try? await item.loadTransferable(type: Data.self) {
-                                    ResultImages.append(image)
+                                    ResultImage.append(image)
                                 }
                             }
                         }
@@ -131,11 +124,11 @@ struct AskAiView: View {
                     .textFieldStyle(.roundedBorder).onKeyPress(.return, action: {
                         UserQuestionSubmitted = UserQuestion
                         UserQuestion = ""
-                        
-                        ResultImagesSubmitted = ResultImages
-                        ResultImages = []
+
+                        ResultImageSubmitted = ResultImage
+                        ResultImage = Data()
                         Task {
-                            try await askAI(Question: UserQuestionSubmitted, Answer: Answer)
+                            try await askAI(Question: UserQuestionSubmitted, Media: ResultImageSubmitted, Answer: Answer)
                         }
                         return .handled
                     })
@@ -144,7 +137,7 @@ struct AskAiView: View {
                     UserQuestionSubmitted = UserQuestion
                     UserQuestion = ""
                     Task {
-                        try await askAI(Question: UserQuestionSubmitted, Answer: Answer)
+                        try await askAI(Question: UserQuestionSubmitted, Media: ResultImageSubmitted, Answer: Answer)
                     }
                 } label: {
                     Image(systemName: "paperplane")
@@ -155,11 +148,19 @@ struct AskAiView: View {
         }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
     }
 
-    func askAI(Question: String, Answer: String) async throws -> String {
-        let response = try await model.generateContent(Question)
-        if let text = response.text {
-            print(text)
-            self.Answer = text
+    func askAI(Question: String, Media: Data, Answer: String) async throws -> String {
+        if !Media.isEmpty {
+            let response = try await model.generateContent(Question, UIImage(data: Media)!)
+            if let text = response.text {
+                print(text)
+                self.Answer = text
+            }
+        } else {
+            let response = try await model.generateContent(Question)
+            if let text = response.text {
+                print(text)
+                self.Answer = text
+            }
         }
         return Answer
     }
@@ -168,18 +169,15 @@ struct AskAiView: View {
 struct AskingAIPreview: PreviewProvider {
     @State static var Question = "Hello, U up"
     @State static var Answer = "This is a test"
-    static var ImagesToData: [Data] {
-        var arr: [Data] = []
-
+    static var ImageToData: Data {
         let image = UIImage(resource: .car)
-        let data = image.jpegData(compressionQuality: 0.5)
-        arr.append(data!)
-
-        return arr
+        let data = image.pngData()
+        return data!
     }
-    @State static var Images: [Data] = ImagesToData
+
+    @State static var ImageSub: Data = ImageToData
 
     static var previews: some View {
-        AskAiView(UserQuestionSubmitted: $Question, Answer: $Answer, ResultImagesSubmitted: $Images)
+        AskAiView(UserQuestionSubmitted: $Question, Answer: $Answer, ResultImageSubmitted: $ImageSub)
     }
 }
