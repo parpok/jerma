6abeb431eee7753d4d4b2f5cf825ffe5739afc86
Patryk <me@parpok.xyz>
2024-07-13 18:17:20 +0200

    SwiftData now works also separation of concens for easier browsing

--- Jerma/ChatView.swift
+++ Jerma/ChatView.swift
@@ -12,7 +12,7 @@ import SwiftUI
 struct ChatView: View {
     @State private var ChosenModel: ModelsAvailble = .gemini_1_5_flash
 
-    @State private var ChatsArray: [[Role: String]] = []
+    @State var ChatsArray: [[Role: String]] = []
 
     var body: some View {
         NavigationStack {
@@ -53,12 +53,12 @@ struct ChatView: View {
 
                 }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 #if targetEnvironment(simulator)
-                    .border(Color.blue)
+//                    .border(Color.blue)
                 #endif
 
                 AskAiView(chatArray: $ChatsArray)
                 #if targetEnvironment(simulator)
-                    .border(Color.green)
+//                    .border(Color.green)
                 #endif
             }.navigationTitle("GermaAiChat")
                 .toolbar {
@@ -89,73 +89,3 @@ struct ChatView: View {
     ChatView()
 }
 
-struct AskAiView: View {
-    @State private var UserQuestion: String = ""
-
-    @State private var UserQuestionSubmitted: String = ""
-    @State private var Answer: String = ""
-
-    @Environment(\.modelContext) private var ModelContext
-
-    @Binding var chatArray: [[Role: String]]
-    var CurrentChat: Chat?
-    var body: some View {
-        VStack {
-            HStack {
-                TextField("Ask something", text: $UserQuestion, axis: .vertical)
-                    .textFieldStyle(.roundedBorder).onKeyPress(.return, action: {
-                        UserQuestionSubmitted = UserQuestion
-                        chatArray.append([.user: UserQuestionSubmitted])
-                        UserQuestion = ""
-
-                        Task {
-                            try await askAI(Question: UserQuestionSubmitted, Answer: Answer)
-                        }
-                        return .handled
-                    })
-
-                Button {
-                    UserQuestionSubmitted = UserQuestion
-                    UserQuestion = ""
-                    Task {
-                        try await askAI(Question: UserQuestionSubmitted, Answer: Answer)
-                    }
-                } label: {
-                    Image(systemName: "paperplane")
-                }.buttonStyle(.borderedProminent)
-
-            }.padding(EdgeInsets(top: 0, leading: 2.5, bottom: 20, trailing: 2.5))
-
-        }.frame(maxWidth: .infinity, alignment: .bottom)
-    }
-
-    func askAI(Question: String, Answer: String) async throws {
-        let response = try await chat.sendMessage(Question)
-        if let text = response.text {
-            print(text)
-            self.Answer = text
-            chatArray.append([.ai: text])
-            save()
-        }
-    }
-
-    private func save() {
-        if var CurrentChat {
-            CurrentChat.chatHistory = chatArray
-        } else {
-            let newChat = Chat(id: UUID(), chatHistory: chatArray)
-            ModelContext.insert(newChat)
-        }
-    }
-}
-
-struct AskingAIPreview: PreviewProvider {
-    @State static var Chats: [[Role: String]] = [[.user: "Hello there"], [.ai: "whats up?"]]
-    @State static var ImageSub: Data = Data()
-    // you know what f that including image. Data will be empty, add image in the preview yourself you lazy F
-    // Thats useless for now
-
-    static var previews: some View {
-        AskAiView(chatArray: $Chats)
-    }
-}
